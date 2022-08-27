187b13a369375b2f738739248a8d4364105101d1
Xayal Suleymani <xayal.suleymani@ibar.az>
2022-08-27 10:02:32 +0400

    remove unnecessary files

--- Calculator/Digit.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Digit.swift
-//  Calculator
-//
-//  Created by khayal on 24.08.22.
-//
-
-import Foundation
--- Calculator/Operation.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Operation.swift
-//  Calculator
-//
-//  Created by khayal on 24.08.22.
-//
-
-import Foundation
-
